62e0cf89ed5151651de1ee190a68c1d20dfce1ff
= <joshkomie@gmail.com>
2018-05-22 17:30:56 -0700

    basic pathfinding

---
+++ Assets/Scripts/WorldView/HeroMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class HeroMove : MonoBehaviour {
+
+	public GameObject Terrain;
+	private NavMeshAgent navMeshAgent;
+
+	public void Start () {
+		navMeshAgent = GetComponent<NavMeshAgent>();
+	}
+	
+	public void Update () {
+		if (Input.GetMouseButton(0)) {
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			if (Terrain.GetComponent<Collider>().Raycast (ray, out hit, Mathf.Infinity)) {
+				navMeshAgent.SetDestination(hit.point);
+			}
+		}
+	}
+}
