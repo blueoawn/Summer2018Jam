69035bb7b1633a83c169a4818c0886a6685e0cb5
= <joshkomie@gmail.com>
2018-05-25 16:50:22 -0700

    make camera follow party leader

---
+++ Assets/Scripts/WorldView/CameraFollow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+  public Vector3 Offset;
+  public Party Party;
+
+  public void Update() {
+    GameObject partyLeader = Party.PartyLeader();
+    transform.position = partyLeader.transform.position + Offset;
+    transform.LookAt(partyLeader.transform);
+  }
+
+}
